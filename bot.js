3b95914d28fb1fae4aee77e9e5410e33e9c5156e
barbosaps <38172199+barbosaps@users.noreply.github.com>
2018-04-08 09:46:53 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,16 @@
 const Discord = require('discord.js');
 const client = new.Discord.Client();
 
-client.on('message', message
+client.on('ready', () => {
+  console.log('I am Ready!');
+});
+
+client.on('message', message => {
+  if (message.content == 'hello'
+      message.reply('hi');
+  }
+  
+});
+
+// THIS MUST BE THIS WAY
+client.login(process.env.BOT_TOKEN);
